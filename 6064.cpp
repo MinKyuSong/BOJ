5bd92c3ebb5a2f8a22321bd8ed80cdb7968aee3a
MinKyu Song <ohh93mk@gmail.com>
2019-11-14 11:54:28 +0900

    Daily Practice, BF.

--- 6064.cpp
+++ 6064.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int x, int y)
+{
+    int a=(x>y)?x:y;
+    int b=(x>y)?y:x;
+    while(b>0)
+    {
+        int temp=a%b;
+        a=b;
+        b=temp;
+    }
+    return a;
+}
+
+long long lcm(int x, int y)
+{
+    return x*y/gcd(x,y);
+}
+
 int main(void)
 {
     int n;
@@ -9,8 +27,18 @@ int main(void)
     {
         int M,N,x,y;
         cin>>M>>N>>x>>y;
-        int m=1,n=1, ans=1;
+        int ans=-1;
+        x-=1;y-=1;
+        long long endDay=lcm(M,N);
         
+        for(int i=x;i<=endDay;i+=M)
+        {
+            if(i%N==y)
+            {
+                ans=i+1;
+                break;
+            }
+        }
         cout<<ans<<endl;
     }
     return 0;
